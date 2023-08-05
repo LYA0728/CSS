50e9f8dbe5be32fa6dce28cd30099710bb56ab1f
yianLiu <89678245+LYA0728@users.noreply.github.com>
2023-08-06 00:10:19 +0800

    Add files via upload

---
+++ clear.js
@@ -0,0 +1,53 @@
+
+$("div.htzxFooter").remove();
+
+$("div.htzxCopyRight").remove();
+
+console.log("成功引入465")
+
+// $(".jiexi-answers").remove();
+
+
+$(".main-topic-letters").remove();
+
+$("div.jiexi-item:first-child").remove();
+
+$("div.jiexi-item:last-child").remove();
+
+
+$("div.tiku-xc-head").remove();
+
+$("div.htzxFooter").remove();
+$("div.g-exercise-head").remove();
+
+
+
+
+
+$("div.show-jiexi-actions").remove();
+
+$("div.exercise-main-title").css({
+    "font-size":"10.5rem",
+    "text-align":" center"
+})
+
+
+$("div").css({
+    "font-size":"1.85rem",
+})
+
+
+
+$("div.exercise-main-title").addClass('text-center');
+
+
+$("div.htzxFooter").remove();
+
+$("div.htzxCopyRight").remove();
+
+$("div.jiexi-item:has(.videoAnalysis)").remove();
+
+
+
+
+
